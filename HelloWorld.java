6cfded28cf3b623807266960a6a24827590dd568
mchanowska <monika.chanowska@gmail.com>
2016-05-29 21:10:21 +0200

    Moja klasa HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld
+{
+
+public static void main (String[] args {
+System.Out.Print("Witam serdecznie. W³aœnie tworzê fork mojego repozytorium, co chwilê uczê siê nowych rzeczy!!");
+}
